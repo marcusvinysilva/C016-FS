15faec2f241f6c7360f94bfc2693994aafe60f2e
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-06-22 16:12:24 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 // import express from 'express';
 const port = 3000;
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World');
